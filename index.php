3c7789525447dcec9623bb951a93826a70d73289
fernando <luferpaniagua27@gamil.com>
2023-11-18 11:48:02 -0500

    codigo

--- index.php
+++ index.php
@@ -3,14 +3,59 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+   
+    <title>portafolio</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
+  <link rel="stylesheet" href="styles.css">
+</head>
   <body>
-    <h1>Hello, world!</h1>
+    <!-- inicio barra de navegacion -->
+    <div class="container">
+  <nav class="navbar navbar-expand-lg">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">LUIS FERNANDO PANIAGUA</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end " id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active"aria-current="page" href="#">Experiencia</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active"aria-current="page" href="#">Proyectos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active"aria-current="page" href="#">Contacto</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>
+<!-- fin barra de navegacion -->
+
+<div class="container text-center">
+  <div class="row row-cols-2">
+    <div class="col-m-6"><img src="profile-pic-2.png"  alt="" class="img rounded float-end" ></div>
+    <div class="col-m-6"><p class="text">Hello,Im</p>
+    <h1>LUIS FERNANDO</h1>
+    <h3>Desarrollo De Sotfware</h3>
+    <button type="button" class="btn btn-primary btn-lg">Download</button>
+<button type="button" class="btn btn-secondary btn-lg">Contact info</button>
+</div>
+  </div>
+</div>
+
+
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+
 </body>
 </html>
 
